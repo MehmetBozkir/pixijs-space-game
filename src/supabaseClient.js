c4c77bd85546468e53d90326a55d60e5b7596b9c
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2024-12-28 19:48:18 +0300

    Create Web Game

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ygiawlwxsqdtfmijrmmo.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnaWF3bHd4c3FkdGZtaWpybW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxNDYxNzMsImV4cCI6MjA1MDcyMjE3M30.bBsumk-UvxeQGVTb5VtDv-9agxmDmSf88T58quI6FI4";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("Supabase environment variables are missing!");
